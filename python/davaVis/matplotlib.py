ecc21a678eb0f99df9851cb9a4cceb9ca211c18a
Ji Won Yeom <jwyeom63@gmail.com>
2020-11-18 08:39:57 +0900

    Update matplotlib.py

--- python/davaVis/matplotlib.py
+++ python/davaVis/matplotlib.py
@@ -12,3 +12,11 @@ ax.plot(seattle_weather["MONTH"], seattle_weather["MLY-PRCP-NORMAL"], marker="o"
 
 # Call the show function to show the result
 plt.show()
+
+# How to show inline
+%matplotlib inline
+
+# Plot yearly proportion of deaths at the two clinics
+ax = yearly1.plot(x="year", y="proportion_deaths", label='clinic 1')
+yearly2.plot(x="year", y="proportion_deaths", label = "clinic 2", ax=ax)
+ax.set_ylabel("Death Proportion")
